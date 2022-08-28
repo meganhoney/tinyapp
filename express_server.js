a8e3160d9e1206e341092296123909d6a6fe2e9f
Megan Honey <mjthoney@gmail.com>
2022-08-28 22:23:37 +0000

    Add cookieparser requirements, add login route to set cookie, add username to templateVars for all page renders

--- express_server.js
+++ express_server.js
@@ -36,16 +36,17 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 })
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
@@ -66,7 +67,7 @@ app.post("/urls", (req, res) => {
 
 app.post("/login", (req, res) => {
   const username = req.body.username;
-  res.cookie(1, username);
+  res.cookie("username", username);
   res.redirect("/urls");
 
 });
