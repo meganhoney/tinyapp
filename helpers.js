ef9af746d50ab67a87939395b21da390514985d6
Megan Honey <mjthoney@gmail.com>
2022-09-06 04:28:40 +0000

    Add all functions to helpers

--- helpers.js
+++ helpers.js
@@ -1,3 +1,12 @@
+const generateRandomString = function() {
+  let randomString = "";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrsthvwxyz1234567890"
+
+  for (let i = 0; i < 6; i++) {
+    randomString += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return randomString;
+};
 
 const getUserByEmail = function(database, email) {
   
@@ -15,6 +24,19 @@ const getUserByEmail = function(database, email) {
   
 };
 
+const urlsForUser = function(id, database) {
+  let urls = {};
+
+  for (let key in database) {
+    if(database[key].userID === id) {
+      urls[key] = database[key].longURL;
+    }
+  }
+  return urls;
+};
+
 module.exports = { 
-  getUserByEmail 
+  generateRandomString,
+  getUserByEmail,
+  urlsForUser,
 };
